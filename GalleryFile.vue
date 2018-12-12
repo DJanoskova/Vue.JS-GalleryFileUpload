1f976a5ab73eab2ca3232d67b980af714e21d8d6
Dana Janoskova <35444313+DJanoskova@users.noreply.github.com>
2018-12-12 07:20:00 +0100

    Create GalleryFile.vue

---
+++ GalleryFile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="gallery-file">
+    <img :src="url"/>
+    <div class="gallery-file__overlay">
+      <i class="el-icon-delete" @click="$emit('detete', id)"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String | Number
+    },
+    url: {
+      type: String
+    }
+  }
+}
+</script>
